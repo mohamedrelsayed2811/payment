c506c7c7bd2e2a68c259c2cf4e15f3ba18673de1
Mohamed Radwan <mohamed.r.elsayed2811@gmail.com>
2022-12-17 21:19:05 +0200

    first commit

--- src/Fawaterk/Api/Fawaterk.php
+++ src/Fawaterk/Api/Fawaterk.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace Radwan\Payments\Fawaterk\Api;
+namespace Radwan\Payment\Fawaterk\Api;
 
 
 
--- src/Fawaterk/Api/FawaterkExecution.php
+++ src/Fawaterk/Api/FawaterkExecution.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Radwan\Payments\Fawaterk\Api;
+namespace Radwan\Payment\Fawaterk\Api;
 
 
 
